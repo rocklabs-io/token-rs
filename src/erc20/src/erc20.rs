683aa837d71cc6664d53175305ee47b984f76b38
ccyanxyz <ccyanxyz@gmail.com>
2021-06-12 16:06:36 +0800

    add impl

--- src/erc20/src/erc20.rs
+++ src/erc20/src/erc20.rs
@@ -28,32 +28,125 @@ fn init(name: String, symbol: String, decimals: u64, total_supply: u64) {
     }
 }
 
-/*
+// TODO: overflow/underflow check
 #[update(name = "transfer")]
-fn transfer() -> bool {
-
+fn transfer(to: Principal, value: u64) -> bool {
+    let from = api::caller();
+    if from == to {
+        return false;
+    }
+    let balances_read = BALANCES.read().unwrap();
+    match balances_read.get(&from) {
+        Some(from_balance) => {
+            if *from_balance < value {
+                false
+            } else {
+                let to_balance = balances_read.get(&to).or_else(|| Some(&0)).unwrap();
+                let mut balances = BALANCES.write().unwrap();
+                balances.insert(from, from_balance - value);
+                balances.insert(to, *to_balance + value);
+                true
+            }
+        },
+        None => false,
+    }
 }
 
 #[update(name = "transferFrom")]
-fn transferFrom() -> bool {
-
+fn transfer_from(from: Principal, to: Principal, value: u64) -> bool {
+    let allowances_read = ALLOWANCES.read().unwrap();
+    match allowances_read.get(&from) {
+        Some(inner) => {
+            match inner.get(&to) {
+                Some(allowance) => {
+                    if *allowance < value {
+                        false
+                    } else {
+                        let balances_read = BALANCES.read().unwrap();
+                        let from_balance = balances_read.get(&from).or_else(|| Some(&0)).unwrap();
+                        let to_balance = balances_read.get(&to).or_else(|| Some(&0)).unwrap();
+                        if *from_balance < value {
+                            false
+                        } else {
+                            let mut balances = BALANCES.write().unwrap();
+                            balances.insert(from, *from_balance - value);
+                            balances.insert(to, *to_balance + value);
+                            true
+                        }
+                    }
+                },
+                None => false,
+            }
+        },
+        None => false,
+    }
 }
 
-#[update(name = "approve")]
-fn approve() -> bool {
 
+// TODO: fix compile error
+#[update(name = "approve")]
+fn approve(spender: Principal, value: u64) -> bool {
+    let owner = api::caller();
+    let allowances_read = ALLOWANCES.read().unwrap();
+    match allowances_read.get(&owner) {
+        Some(inner) => {
+            inner.insert(spender, value);
+            let mut allowances = ALLOWANCES.write().unwrap();
+            allowances.insert(owner, inner);
+        },
+        None => {
+            let mut inner = HashMap::new();
+            inner.insert(spender, value);
+            let mut allowances = ALLOWANCES.write().unwrap();
+            allowances.insert(owner, inner);
+        }
+    }
+    true
 }
 
 #[update(name = "mint")]
-fn mint() -> bool {
-
+fn mint(to: Principal, value: u64) -> bool {
+    if api::caller() != to {
+        false
+    } else {
+        let balances_read = BALANCES.read().unwrap();
+        let balance_before = balances_read.get(&to).or_else(|| Some(&0)).unwrap();
+        if balance_before + value >= u64::MAX {
+            false
+        } else {
+            let mut balances = BALANCES.write().unwrap();
+            balances.insert(to, balance_before + value);
+            unsafe {
+                TOTALSUPPLY += value;
+            }
+            true
+        }
+    }
 }
 
 #[update(name = "burn")]
-fn burn() -> bool {
-
+fn burn(from: Principal, value: u64) -> bool {
+    if api::caller() != from || api::caller() != owner() {
+        false
+    } else {
+        let balances_read = BALANCES.read().unwrap();
+        match balances_read.get(&from) {
+            Some(balance) => {
+                if *balance < value {
+                    false
+                } else {
+                    let mut balances = BALANCES.write().unwrap();
+                    balances.insert(from, balance - value);
+                    unsafe {
+                        TOTALSUPPLY -= value;
+                    }
+                    true
+                }
+            },
+            None => false,
+        }
+    }
 }
-*/
 
 #[query(name = "balanceOf")]
 fn balance_of(id: Principal) -> u64 {
